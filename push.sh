9b00bf5bd68b7a6eb3ad517d054e5162d18de698
iosonoumberto <root@localhost.localdomain>
2024-01-23 02:37:55 -0800

    Tue Jan 23 02:37:55 PST 2024

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git config --global user.name "iosonoumberto"
-git config --global user.password "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"
+git config --global user.password "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"
 git config credential.helper store
 
 git add .
