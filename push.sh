2399a93b2349957566f5fa05d71591f699942084
iosonoumberto <root@localhost.localdomain>
2024-01-23 02:27:35 -0800

    Tue Jan 23 02:27:35 PST 2024

--- push.sh
+++ push.sh
@@ -1,3 +1,7 @@
+git config --global user.name "iosonoumberto"
+git config --global user.password "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"
+git config credential.helper store
+
 git add .
 git commit -m "$(date)"
 git push origin main
